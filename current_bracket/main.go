ca58e0e0db68a29b46c63eb931bae2354315c28a
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2023-02-23 20:50:44 -0500

    Create Bracket Structs

--- current_bracket/main.go
+++ current_bracket/main.go
@@ -6,8 +6,24 @@ import (
 	"example.com/api/app"
 )
 
+type Member struct {
+	name string
+}
+
+type Match struct {
+	member1     Member
+	member2     Member
+	member1Wins uint
+	member2Wins uint
+}
+
+type singleBracketEvent struct {
+	matches   []Match
+	standings []Member
+}
+
 func main() {
-	fmt.Println("Hello! This is working correctly!")
+	fmt.Println("Hello!")
 	app := &app.App{}
 	app.Initialize()
 	app.Run(":3000")
