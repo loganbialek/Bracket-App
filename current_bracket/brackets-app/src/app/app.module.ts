3ba3e0eac9ba66a7114aea33e4e3b4cab5eb0a72
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2023-02-27 15:22:02 -0500

    bracket initialization

--- current_bracket/brackets-app/src/app/app.module.ts
+++ current_bracket/brackets-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     MatSlideToggleModule,
-    MatTableModule
+    MatTableModule,
+    BrowserAnimationsModule
      
     
   ],
--- current_bracket/main.go
+++ current_bracket/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"example.com/api/app"
 )
@@ -17,12 +18,28 @@ type Match struct {
 	member2Wins uint
 }
 
-type singleBracketEvent struct {
-	matches   []Match
-	standings []Member
-}
-
 func main() {
+	fmt.Print("Type a number of rounds: ")
+	var rounds float64
+
+	fmt.Print("Type a number of rounds: ")
+	fmt.Scan(&rounds)
+	rounds = math.Pow(2, math.Ceil(math.Log(rounds)/math.Log(2)))
+
+	var r int = int(rounds)
+	var matches = make([]Match, r)
+
+	UNINITIALIZED_MEMBER := Member{name: "UNINITIALIZED"}
+	UNINITIALIZED_MATCH := Match{member1: UNINITIALIZED_MEMBER, member2: UNINITIALIZED_MEMBER, member1Wins: 0, member2Wins: 0}
+
+	for i := 0; i < r; i++ {
+		matches = append(matches, UNINITIALIZED_MATCH)
+	}
+
+	for i := 0; i < r; i++ {
+		fmt.Println(matches[i].member1.name + " " + matches[i].member2.name)
+	}
+
 	fmt.Println("Hello!")
 	app := &app.App{}
 	app.Initialize()
